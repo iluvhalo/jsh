42dfeb7936d9ad05f1fe43a3edc019ecc4b80736
iluvhalo <hgd145@vols.utk.edu>
2018-04-04 20:04:16 -0400

    GS 1-9 work

--- jsh.c
+++ jsh.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <fields.h>
+#include <sys/wait.h>
 
 int main (int argc, char **argv) {
-  int i;
+  int i, j;
   int status;
+  int amp;
   char *prompt;
   char **newarg;
   IS is;
@@ -37,20 +39,43 @@ int main (int argc, char **argv) {
 
   // this is the main loop of the shell
   is = new_inputstruct(NULL);
+  here:
   printf("%s", prompt);
   while (get_line(is) != -1) {
+    if (is->NF == 0) goto here;
     // if calling exit, closes the shell
-    if (strcmp(is->fields[0], "exit") == 0) exit(0);
+    // also added q, just for a faster way for when I'm testing
+    if ((strcmp(is->fields[0], "exit") == 0) || (strcmp(is->fields[0], "q") == 0)) exit(0);
     
-    newarg = (char **) malloc(sizeof(char *) * (is->NF + 1));
+    // checks if there is a '&' at the end of the command
+    amp = 0;
+    if (strcmp(is->fields[is->NF - 1], "&") == 0) {
+//      printf("there is an &\n");
+      amp = 1;
+    }
+    
+    // allocates the coreect amount of args for newargs
+    if (amp) {
+      newarg = (char **) malloc(sizeof(char *) * is->NF);
+    } else {
+      newarg = (char **) malloc(sizeof(char *) * (is->NF + 1));
+    }
 
     // fills the newarg array with the arguments to pass to exec
-    for (i = 0; i < is->NF; i++) {
-      newarg[i] = strdup(is->fields[i]);
-//      printf("copying %s from is->fields into newarg\n", newarg[i]);
+    if  (!amp) {
+      for (i = 0; i < is->NF; i++) {
+        newarg[i] = strdup(is->fields[i]);
+      }
+    
+      // NULL terminator for newarg array
+      newarg[is->NF] = NULL;
+    } else {
+      for (i = 0; i < (is->NF - 1); i++) {
+        newarg[i] = strdup(is->fields[i]);
+      }
+
+      newarg[is->NF - 1] = NULL;
     }
-    // NULL terminator for newarg array
-    newarg[is->NF] = NULL;
 
     // prints newarg to make sure it filled correctly
     for (i = 0; i < is->NF; i++) {
@@ -63,7 +88,9 @@ int main (int argc, char **argv) {
       perror(newarg[0]);
       exit(1);
     } else {
-      wait(&status);
+      if (!amp) {
+        wait(&status);
+      }
     }
 
 
